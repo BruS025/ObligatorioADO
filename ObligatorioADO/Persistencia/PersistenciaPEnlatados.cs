1b80f1d549399e184a5c5371a4b77b572b3aad6f
ValeRivero <riverovaleria5@gmail.com>
2016-11-24 19:23:54 -0300

    Clases y referencias

--- ObligatorioADO/Persistencia/PersistenciaPEnlatados.cs
+++ ObligatorioADO/Persistencia/PersistenciaPEnlatados.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntidadesCompartidas;
 
 namespace Persistencia
 {
-    public class Class1
+    class PersistenciaPEnlatados
     {
     }
 }
--- ObligatorioADO/Persistencia/PersistenciaVentas.cs
+++ ObligatorioADO/Persistencia/PersistenciaVentas.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using EntidadesCompartidas;
 
-namespace Logica1
+namespace Persistencia
 {
-    class LogicaClientes
+    class PersistenciaVentas
     {
     }
 }
