44092957aa2aeda5834810e41066e020fed75f4a
Rodrigo Antognazza <rantognazza@gmail.com>
2016-11-24 16:30:36 -0300

    def

--- ObligatorioADO/Presentacion/Default.aspx.cs
+++ ObligatorioADO/Presentacion/Default.aspx.cs
@@ -9,9 +9,26 @@ namespace Presentacion
 {
     public partial class Default : System.Web.UI.Page
     {
+        // Page load
         protected void Page_Load(object sender, EventArgs e)
         {
                 
+        }
+
+        // Redirects
+        protected void btMenuMantenimientoClientes_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MantenimientoClientes.aspx");
+        }
+
+        protected void btMenuMantenimientoProductosEnlatados_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MantenimientoProductosEnlatados.aspx");
+        }
+
+        protected void btMenuMantenimientoProductosCongelados_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MantenimientoProductosCongelados.aspx");
         }
     }
 }
