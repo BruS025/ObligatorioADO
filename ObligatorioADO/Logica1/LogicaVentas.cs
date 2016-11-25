2e4517cc0164843aa35c4ed88a514f7a53642b22
Rodrigo Antognazza <rantognazza@gmail.com>
2016-11-25 14:39:22 -0300

    Update LogicaVentas.cs

--- ObligatorioADO/Logica1/LogicaVentas.cs
+++ ObligatorioADO/Logica1/LogicaVentas.cs
@@ -9,6 +9,29 @@ namespace Logica
 {
     public class LogicaVentas
     {
+        public static List<Venta> Buscar(String busqueda)
+        {
+            return PersistenciaVentas.Buscar(String busqueda);
+        }
 
+        public static int Agregar(Venta venta)
+        {
+            return PersistenciaVentas.Agregar(venta);
+        }
+
+        public static int Modificar(Venta venta)
+        {
+            return PersistenciaVentas.Modificar(venta);
+        }
+
+        public static int Eliminar(int id)
+        {
+            return PersistenciaVentas.Eliminar(id);
+        }
+        
+        public static List<Venta> Listar()
+        {
+            return PersistenciaVentas.Listar();
+        }
     }
 }
